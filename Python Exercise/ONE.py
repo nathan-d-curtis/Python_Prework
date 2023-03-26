173d4e498db9cc50c20a892a5a2ee24a4ff26bbd
Nathan D. Curtis <127367168+nathan-d-curtis@users.noreply.github.com>
2023-03-25 19:39:46 -0500

    Update ONE.py

--- Python Exercise/ONE.py
+++ Python Exercise/ONE.py
@@ -1,26 +1,18 @@
 # Question 1 - Write a function to print "Hello_USERNAME!"
 
 def hello_name(user_name):
-    print("Hello" + user_name.upper() + "!")
+    print("Hello " + user_name.upper() + "!")
     
 hello_name('Nathan')
 
 
 # Question 2 - Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing.
 
-def odd_numbers():
+def first_odds():
     for i in range(1,101,2):
         print(i)
         
-#odd_numbers()
-        
-def odd_numbers2():
-  	numbers = list(range(0, 101))
-    for number in numbers:
-        if number % 2 != 0:
-        	print(number)
-        
-odd_numbers2()
+first_odds()
 
 
 # Question 3 - Please write a Python function, max_num_in_list to return the max number of a given list.
@@ -29,8 +21,7 @@ def max_num_in_list(a_list):
     max_num = max(a_list)
     return max_num
 
-test = max_num_in_list([2,3,5,8,9])
-print(max_num_in_list([2,3,5,8,9]))
+print(max_num_in_list([1,2,3,5,8,9,11]))
 
 
 # Question 4 - Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400.
@@ -43,6 +34,8 @@ def is_leap_year(a_year):
     else:
         a_year = False
         print(f'{a_year}')
+
+is_leap_year(2028)
 
 
 # Question 5 - Write a function to check to see if all numbers in list are consecutive numbers.
@@ -57,4 +50,6 @@ def is_consecutive(a_list):
             status = False
             break
     print(status)
+
+is_consecutive([1,2,3,4,5,6,7,8,9])
 
